532a75ef85254ff0e7b8a9c24616763a63869af6
Mi13285 <mixkud95@inbox.ru>
2024-01-13 13:47:40 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+// function hellomam(){
+//     console.log("hello")
+// }
+// hellomam()
+
+
+// let helloSlava=function(){
+//   console.log  ("hello")
+// }
+// helloSlava()
+
+// let admin="misha"
+// console.log(admin)
+
+// debugger;
+// let name =prompt('Ввидите имя')
+// function hi(){
+//     if(name==="Slava"){
+//         console.log('LOL')
+//     }
+//     else{
+//         console.log("pop")
+//     }
+// }
+// hi()
+
+
+// let summa=(a,b)=>a+b
+// console.log(summa(100,50))
